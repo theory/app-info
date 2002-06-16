f89de72482fe508c0523b388fc9855ce7206ac47
David E. Wheeler <david@kineticode.com>
2002-06-16 02:09:31 +0000

    Documented Carp and Print.

--- lib/App/Info/Handler/Carp.pm
+++ lib/App/Info/Handler/Carp.pm
@@ -1,22 +1,65 @@
 package App::Info::Handler::Carp;
 
-# $Id: Carp.pm,v 1.5 2002/06/15 00:49:55 david Exp $
+# $Id: Carp.pm,v 1.6 2002/06/16 02:09:31 david Exp $
 
 =head1 NAME
 
-App::Info::Handler::Carp - App::Info Carp handler
+App::Info::Handler::Carp - Use Carp to handle App::Info events
 
 =head1 SYNOPSIS
 
-  use strict;
-  use App::Info::Handler::Carp;
   use App::Info::Category::FooApp;
+  use App::Info::Handler::Carp;
 
-  my $carp = App::Info::Handler::Carp->new('croak');
+  my $carp = App::Info::Handler::Carp->new('carp');
+  my $app = App::Info::Category::FooApp->new( on_info => $carp );
+
+  # Or...
+  my $app = App::Info::Category::FooApp->new( on_error => 'croak' );
 
 =head1 DESCRIPTION
 
-To be written.
+App::Info::Handler::Carp objects handle App::Info events by passing their
+messages to Carp functions. This means that if you want errors to croak or
+info messages to carp, you can easily do that. You'll find, however, that
+App::Info::Handler::Carp is most effective for info and error events; unknown
+and prompt events are better handled by event handlers that know how to prompt
+users for data. See L<App::Info::Handler::Prompt|App::Info::Handler::Prompt>
+for an example of that functionality.
+
+Upon loading, App::Info::Handler::Carp registers itself with
+App::Info::Handler, setting up a number of strings that can be passed to an
+App::Info concrete subclass constructor. These strings are shortcuts that
+tell App::Info how to create the proper App::Info::Handler::Carp object
+for handling events. The registered strings are:
+
+=over
+
+=item carp
+
+Passes the event message to C<Carp::carp()>.
+
+=item warn
+
+An alias for "carp".
+
+=item croak
+
+Passes the event message to C<Carp::croak()>.
+
+=item die
+
+An alias for "croak".
+
+=item cluck
+
+Passes the event message to C<Carp::cluck()>.
+
+=item confess
+
+Passes the event message to C<Carp::confess()>.
+
+=back
 
 =cut
 
@@ -41,6 +84,58 @@ for my $c (qw(croak carp cluck confess die warn)) {
     App::Info::Handler->register_handler
       ($c => sub { __PACKAGE__->new( level => $c ) } );
 }
+
+=head1 INTERFACE
+
+=head2 Constructor
+
+=head3 new
+
+  my $carp_handler = App::Info::Handler::Carp->new;
+  $carp_handler = App::Info::Handler::Carp->new( level => 'carp' );
+  my $croak_handler = App::Info::Handler::Carp->new( level => 'croak' );
+
+Constructs a new App::Info::Handler::Carp object and returns it. It can take a
+single parameterized argument, C<level>, which can be any one of the following
+values:
+
+=over
+
+=item carp
+
+Constructs a App::Info::Handler::Carp object that passes the event message to
+C<Carp::carp()>.
+
+=item warn
+
+An alias for "carp".
+
+=item croak
+
+Constructs a App::Info::Handler::Carp object that passes the event message to
+C<Carp::croak()>.
+
+=item die
+
+An alias for "croak".
+
+=item cluck
+
+Constructs a App::Info::Handler::Carp object that passes the event message to
+C<Carp::cluck()>.
+
+=item confess
+
+Constructs a App::Info::Handler::Carp object that passes the event message to
+C<Carp::confess()>.
+
+=back
+
+If the C<level> parameter is not passed, C<new()> will default to creating an
+App::Info::Handler::Carp object that passes App::Info event messages to
+C<Carp::carp()>.
+
+=cut
 
 sub new {
     my $pkg = shift;
@@ -69,8 +164,7 @@ __END__
 
 =head1 BUGS
 
-Can there really be much in the way of bugs in an abstract base class? Drop me
-a line if you happen to discover any.
+Feel free to drop me an email if you discover any bugs. Patches welcome.
 
 =head1 AUTHOR
 
@@ -78,11 +172,18 @@ David Wheeler <david@wheeler.net>
 
 =head1 SEE ALSO
 
-L<App::Info|App::Info>
-L<App::Info::HTTPD::Apache|App::Info::HTTPD::Apache>,
-L<App::Info::RDBMS::PostgreSQL|App::Info::RDBMS::PostgreSQL>,
-L<App::Info::Lib|App::Info::Lib::Expat>,
-L<App::Info::Lib|App::Info::Lib::Iconv>
+L<App::Info|App::Info> documents the event handling interface.
+
+L<Carp|Carp> of documents the functions used by this class.
+
+L<App::Info::Handler::Print|App::Info::Handler::Print> handles events by
+printing their messages to a file handle.
+
+L<App::Info::Handler::Prompt|App::Info::Handler::Prompt> offers event handling
+more appropriate for unknown and confirm events.
+
+L<App::Info::Handler|App::Info::Handler> describes how to implement custom
+App::Info event handlers.
 
 =head1 COPYRIGHT AND LICENSE
 
--- lib/App/Info/Handler/Print.pm
+++ lib/App/Info/Handler/Print.pm
@@ -1,22 +1,53 @@
 package App::Info::Handler::Print;
 
-# $Id: Print.pm,v 1.3 2002/06/15 00:49:55 david Exp $
+# $Id: Print.pm,v 1.4 2002/06/16 02:09:31 david Exp $
 
 =head1 NAME
 
-App::Info::Handler::Print - App::Info Print handler
+App::Info::Handler::Print - Print App::Info event messages
 
 =head1 SYNOPSIS
 
-  use strict;
-  use App::Info::Handler::Print;
   use App::Info::Category::FooApp;
+  use App::Info::Handler::Print;
 
-  my $carp = App::Info::Handler::Print->new('stderr');
+  my $stdout = App::Info::Handler::Print->new( fh => 'stdout' );
+  my $app = App::Info::Category::FooApp->new( on_info => $stdout );
+
+  # Or...
+  my $app = App::Info::Category::FooApp->new( on_error => 'stderr' );
 
 =head1 DESCRIPTION
 
-To be written.
+App::Info::Handler::Print objects handle App::Info events by printing their
+messages to a filehandle. This means that if you want event messages to print
+to a file or to a system filehandle, you can easily do it with this class.
+You'll find, however, that App::Info::Handler::Print is most effective for
+info and error events; unknown and prompt events are better handled by event
+handlers that know how to prompt users for data. See
+L<App::Info::Handler::Prompt|App::Info::Handler::Prompt> for an example of
+that functionality.
+
+Upon loading, App::Info::Handler::Print registers itself with
+App::Info::Handler, setting up a couple of strings that can be passed to an
+App::Info concrete subclass constructor. These strings are shortcuts that
+tell App::Info how to create the proper App::Info::Handler::Print object
+for handling events. The registered strings are:
+
+=over 4
+
+=item stdout
+
+Prints event messages to C<STDOUT>.
+
+=item stderr
+
+Prints event messages to C<STDERR>.
+
+=back
+
+See the C<new()> constructor below for how to have App::Info::Handler::Print
+print event messages to different filehandle.
 
 =cut
 
@@ -32,6 +63,51 @@ for my $c (qw(stderr stdout)) {
       ($c, sub { __PACKAGE__->new( fh => $c ) } );
 }
 
+=head1 INTERFACE
+
+=head2 Constructor
+
+=head3 new
+
+  my $stderr_handler = App::Info::Handler::Print->new;
+  $stderr_handler = App::Info::Handler::Print->new( fh => 'stderr' );
+  my $stdout_handler = App::Info::Handler::Print->new( fh => 'stdout' );
+  my $fh = FileHandle->new($file);
+  my $fh_handler = App::Info::Handler::Print->new( fh => $fh );
+
+Constructs a new App::Info::Handler::Print and returns it. It can take a
+single parameterized argument, C<fh>, which can be any one of the following
+values:
+
+=over 4
+
+=item stderr
+
+Constructs a App::Info::Handler::Print object that prints App::Info event
+messages to C<STDERR>.
+
+=item stdout
+
+Constructs a App::Info::Handler::Print object that prints App::Info event
+messages to C<STDOUT>.
+
+=item FileHandle
+
+=item GLOB
+
+Pass in a reference and App::Info::Handler::Print will assume that it's a
+filehandle reference that it can print to. Note that passing in something that
+can't be printed to will trigger an exception when App::Info::Handler::Print
+tries to print to it.
+
+=back
+
+If the C<fh> parameter is not passed, C<new()> will default to creating an
+App::Info::Handler::Print object that prints App::Info event messages to
+C<STDOUT>.
+
+=cut
+
 sub new {
     my $pkg = shift;
     my $self = $pkg->SUPER::new(@_);
@@ -42,7 +118,7 @@ sub new {
         # Create a reference to STDOUT.
         $self->{fh} = \*STDOUT;
     } elsif (!ref $self->{fh}) {
-        # Assume a reference to a file handle or else it's invalid.
+        # Assume a reference to a filehandle or else it's invalid.
         Carp::croak("Invalid argument to new(): '$self->{fh}'");
     }
     # We're done!
@@ -61,8 +137,7 @@ __END__
 
 =head1 BUGS
 
-Can there really be much in the way of bugs in an abstract base class? Drop me
-a line if you happen to discover any.
+Feel free to drop me an email if you discover any bugs. Patches welcome.
 
 =head1 AUTHOR
 
@@ -70,11 +145,16 @@ David Wheeler <david@wheeler.net>
 
 =head1 SEE ALSO
 
-L<App::Info|App::Info>
-L<App::Info::HTTPD::Apache|App::Info::HTTPD::Apache>,
-L<App::Info::RDBMS::PostgreSQL|App::Info::RDBMS::PostgreSQL>,
-L<App::Info::Lib|App::Info::Lib::Expat>,
-L<App::Info::Lib|App::Info::Lib::Iconv>
+L<App::Info|App::Info> documents the event handling interface.
+
+L<App::Info::Handler::Carp|App::Info::Handler::Carp> handles events by
+passing their messages Carp module functions.
+
+L<App::Info::Handler::Prompt|App::Info::Handler::Prompt> offers event handling
+more appropriate for unknown and confirm events.
+
+L<App::Info::Handler|App::Info::Handler> describes how to implement custom
+App::Info event handlers.
 
 =head1 COPYRIGHT AND LICENSE
 
