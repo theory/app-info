3f5df2d4d7413298d314b653d25aa44c19fbbe15
David E. Wheeler <david@kineticode.com>
2006-09-16 00:01:20 +0000

    Updated copyright year.

--- lib/App/Info/HTTPD.pm
+++ lib/App/Info/HTTPD.pm
@@ -68,7 +68,7 @@ L<App::Info::HTTPD::Apache|App::Info::HTTPD::Apache>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2002-2004, David Wheeler. All Rights Reserved.
+Copyright (c) 2002-2006, David Wheeler. All Rights Reserved.
 
 This module is free software; you can redistribute it and/or modify it under the
 same terms as Perl itself.
--- lib/App/Info/HTTPD/Apache.pm
+++ lib/App/Info/HTTPD/Apache.pm
@@ -1705,7 +1705,7 @@ L<http://perl.apache.org/> is the mod_perl home page.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2002-2004, David Wheeler. All Rights Reserved.
+Copyright (c) 2002-2006, David Wheeler. All Rights Reserved.
 
 This module is free software; you can redistribute it and/or modify it under the
 same terms as Perl itself.
