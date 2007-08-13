cdba422055f5ca102c355d5bd51c392dcb12272d
David E. Wheeler <david@kineticode.com>
2007-08-13 18:26:53 +0000

    Incremented version number to 0.53.

--- lib/App/Info/Handler/Prompt.pm
+++ lib/App/Info/Handler/Prompt.pm
@@ -45,7 +45,7 @@ events.
 use strict;
 use App::Info::Handler;
 use vars qw($VERSION @ISA);
-$VERSION = '0.52';
+$VERSION = '0.53';
 @ISA = qw(App::Info::Handler);
 
 # Register ourselves.
--- lib/App/Info/Request.pm
+++ lib/App/Info/Request.pm
@@ -45,7 +45,7 @@ to handle the request appropriately.
 use strict;
 use vars qw($VERSION);
 use Carp;
-$VERSION = '0.52';
+$VERSION = '0.53';
 
 ##############################################################################
 
